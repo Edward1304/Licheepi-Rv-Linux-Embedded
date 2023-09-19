7ff8685e8d4f95b36ff83f37722bb8112dc3fdd0
Edward1304 <egoyeneche@unal.edu.co>
2023-09-18 21:31:02 -0400

    update firs build Remote host  by serial connection

---
+++ cmake-build-debug/CMakeFiles/Remote_Test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Remote_Test.dir/main.c.o"
+  "CMakeFiles/Remote_Test.dir/main.c.o.d"
+  "Remote_Test"
+  "Remote_Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Remote_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
